e91328e644c31b1b0e7ae28f85d9b7abcc10938f
ArturNicolaiewskyPellicioli <pellicioli.artur@gmail.com>
2022-06-24 13:41:07 -0300

    fix: consoles added

--- src/services/getUserData.jsx
+++ src/services/getUserData.jsx
@@ -2,16 +2,22 @@ import {collection, getDocs, query, where} from 'firebase/firestore';
 import {database} from '../configs/firebase';
 
 export const getUserData = async (userId) => {
+  console.log('userId', userId);
   const userDataQuery = query(
     collection(database, 'users'),
     where('uid', '==', userId),
   );
 
+  console.log('userDataQuery', userDataQuery);
   const querySnapshot = await getDocs(userDataQuery);
+
+  console.log('querySnapshot', querySnapshot);
 
   const userDoc = querySnapshot.forEach((snap) => {
     return snap.data();
   });
+
+  console.log('userDoc', userDoc);
 
   return userDoc;
 };
