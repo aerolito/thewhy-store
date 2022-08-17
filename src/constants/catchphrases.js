1108af8eb97dda2df74969a113c1fdb0fc2ec9c6
ArturNicolaiewskyPellicioli <pellicioli.artur@gmail.com>
2022-08-17 12:42:43 -0300

    fixes

--- src/constants/catchphrases.js
+++ src/constants/catchphrases.js
@@ -9,7 +9,10 @@ export const catchphrases = [
    consumo e consumismo é uma 
    das essências da nova economia.`,
   `A compra por impulso é boa para quem?`,
-  `E se todas as plataformas fossem como a The Why Store, e ajudassem você a tomar consciência durante o processo de compra?`,
+  `E se todas as plataformas fossem
+   como a The Why Store, e ajudassem você
+   a tomar consciência durante
+   o processo de compra?`,
   `Compre menos. Compre melhor. 
    E se estiver na dúvida, talvez nem seja 
    o caso de comprar.`,
